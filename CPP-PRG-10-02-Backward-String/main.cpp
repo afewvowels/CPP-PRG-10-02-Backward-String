052a738470e69dd275f6be13030fda51fb3f9891
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-01 15:08:53 -0400

    Prompt written.

--- CPP-PRG-10-02-Backward-String/main.cpp
+++ CPP-PRG-10-02-Backward-String/main.cpp
@@ -5,11 +5,17 @@
 //  Created by Keith Smith on 11/1/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a function that accepts a pointer to a C-string as an argument and displays its
+//  contents backwards. For instance, if the string argument is "Gravity" the function
+//  should display "ytivarG". Demonstrate the function in a program that asks the user
+//  to input a string then passes it to the function.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
     return 0;
 }
